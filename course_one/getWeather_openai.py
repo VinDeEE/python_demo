b4dc503ce2d292cde145e31603a19f5b01083f72
fandi <fandi@fcbox.com>
2026-02-28 14:58:15 +0800

    demo提交

---
+++ course_one/getWeather_openai.py
@@ -0,0 +1,22 @@
+import getpass
+import os
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter OPENAI API Key: ")
+
+from langchain.agents import create_agent
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="openai:gpt-4o-mini",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
